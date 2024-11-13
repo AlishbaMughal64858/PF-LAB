287e30a7c563d6363100cd76909b0348e47c3f2c
AlishbaMughal64858 <64858@students.riphah.edu.pk>
2024-11-13 01:14:40 -0800

    Add files via upload

---
+++ array task 2D sum.cpp
@@ -0,0 +1,35 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int rows,column;
+	int sum=0;
+	cout<<"enter the number of rows: ";
+	cin>>rows;
+	cout<<"enter the number of column: ";
+	cin>>column;
+	
+	//Declare a 2D array;
+	int arr[rows][column];
+	cout<<"enter elements of the array:\n";
+	for(int i=0; i<rows; i++){
+	 for(int j=0; j<column; j++){
+	 	cout<<"element[ "<<i<< "]["<<j<<"]";
+	 	cin>>arr[i][j];
+	 	
+	 	 sum=sum+arr[i][j];
+		  }
+	    
+	   	 	
+	 
+}
+	 //display the 2D array
+	 cout<<" \nthe 2D array is:\n";
+	 for(int i=0; i<rows; i++){ 
+	 for(int j=0; j<column; j++){
+	 	cout<<arr[i][j] <<" ";
+	 }
+	 }
+	    cout<<"\n the sum is: ";
+ 
+         return 0;}
